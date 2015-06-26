cac5d08d61e7c9a73893953a88d42e67320415f9
Andrew Ettinger <andrewmettinger@gmail.com>
2015-06-26 12:21:58 -0400

    2nd iteration: Searching returns pictures of posters

--- main.rb
+++ main.rb
@@ -1,13 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'imdb'
 
 enable(:sessions)
+
+searched_title = []
 
 get '/' do
 	erb(:search)
 end
 
+post '/question' do
+	@search = Imdb::Search.new(params[:search])
+	erb(:question)
+end
+
+
 #post 9 movies + question
 
 #post checks answer
 
+#get shows result for answer 
